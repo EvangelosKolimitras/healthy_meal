096b11d133e057caa199b2cc93f3fb487a5735a5
Evangelos_Kolimitras <evangeloskolimitras@outlook.com>
2019-02-04 14:00:58 +0100

    Application basic set up updated

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,8 @@ module.exports = {
 
     plugins : [ 
         new htmlWebpackPlugin({
-            template : './src/index.html'
+            template : './src/index.html',
+            hash : true
         })
     ],
 
