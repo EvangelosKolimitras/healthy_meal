8692ab536e8ef2a9e82fa540131c97071c7fe809
Evangelos_Kolimitras <evangeloskolimitras@outlook.com>
2019-02-04 15:01:30 +0100

    Packages added
    +duplicate-package-checker-webpack-plugin
    +file-loader"

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,76 @@
 const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin');
+const DuplicatePackageCheckerPlugin = require("duplicate-package-checker-webpack-plugin");
+
 
 module.exports = {
 
     // Mode configuration
     // development or production
-    mode : 'development',
-    
+    mode: 'development',
+
 
     //Server Configurations
-    devServer : {
-        contentBase : path.join( __dirname , 'dist'),
-        compress : true,
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
         port: 9000
     },
 
     // Input & Output
-    entry : [
+    entry: [
         './src/js/app.js'
     ],
 
-    output : {
-        path : path.resolve( __dirname , 'dist'),
-        filename : './src/js/bundle.js'
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: './src/js/bundle.js'
     },
 
     // Plugins
 
-    plugins : [ 
+    plugins: [
+
         new htmlWebpackPlugin({
-            template : './src/index.html',
-            hash : true
+            template: './src/index.html',
+            hash: true
+        }),
+
+        new DuplicatePackageCheckerPlugin({
+            emitError: true, // Emit errors instead of warnings (default: false)
+            strict: false// Warn also if major versions differ (default: true)
         })
+
     ],
 
     module: {
         rules: [
-            {   test: /\.js$/,
+            {
+                test: /\.js$/,
                 exclude: /node_modules/,
                 use: ["babel-loader"]
             },
-            
+
             {
-                test : /\.(sc|sa|c)ss$/,
+                test: /\.(sc|sa|c)ss$/,
                 use: [
                     'style-loader',
                     'css-loader',
                     'sass-loader'
                 ]
+            },
+
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {},
+                    },
+                ],
             }
+            
         ]
-      }
-        
+    }
+
 };
